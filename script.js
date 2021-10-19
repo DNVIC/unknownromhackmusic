8a02cf07c89728cc07b594c0e014772935b4b0b2
DNVIC <37513362+DNVIC@users.noreply.github.com>
2021-10-19 11:10:27 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+$(function() {
+	$("#info").click(function () {
+  	document.location.href = "https://dnvic.com/unknownromhackmusic/info.html"
+  })
+  $("#main").click(function () {
+  	document.location.href = "https://dnvic.com/unknownromhackmusic/info.html"
+  })
+})
